a4907e04de358a572fe3e5b4c4062ba2538c9caa
Ajit <ajitpathak0448@gmail.com>
2022-12-21 20:03:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   console.log('abc')
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Chutiyooo
+          Learn React
         </a>
 
       </header>
